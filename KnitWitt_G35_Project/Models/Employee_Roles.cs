cec725cd222ead0a52ae55883366700e6ed1f02d
Syed Fraz <frazsyed98@gmail.com>
2023-04-18 23:26:15 +0500

    Real Domain Classes created

---
+++ KnitWitt_G35_Project/Models/Employee_Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace KnitWitt_G35_Project.Models
+{
+    public class Employee_Roles:Employees_interface, Roles_interface
+    {
+        public int Employee_Id { get; set; }
+        public int Role_Id { get; set; }
+        public DateOnly Start_Date { get; set; }
+        public DateOnly End_Date { get; set; }
+    }
+}
