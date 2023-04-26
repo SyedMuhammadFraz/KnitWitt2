52a7c763095b0f44f2b5dee98198f7f75d37232e
Umair Saleem <Umair Saleem@DESKTOP-CC12B55>
2023-04-26 18:39:28 +0500

    a

--- react-app/src/Pages/Read.js
+++ react-app/src/Pages/Read.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+
+
 const Read = () => {
   const [data, setData] = useState([]);
   const [tabledark, setTableDark] = useState("");
